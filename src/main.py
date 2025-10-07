284c86d599cb1eac6863924b78633783ccedbe4f
leonardojdss <leonardojpsedasilva@gmail.com>
2025-10-07 01:30:12 -0300

    implementado langfuse

--- src/main.py
+++ src/main.py
@@ -1,6 +1,10 @@
 from src.workflow.graph import Graph
-
+from langfuse.langchain import CallbackHandler
+ 
+langfuse_handler = CallbackHandler()
 graph = Graph()
 
-state = graph.builder_workflow().invoke({})
+initial_state = {}
+
+state = graph.builder_workflow().invoke(initial_state, config={"callbacks": [langfuse_handler]})
 print(state["combined_output"])
